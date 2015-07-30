38ef4c539951d4e594962846dca31a93eaf94a1d
Samantha Sunders <samanthasunders@gmail.com>
2015-07-30 09:00:31 -0500

    Changes the button to append

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('body').prepend("<button id = 'clickyButton'>See all my wonderful Talents!</button>");
+    $('body').append("<button id = 'clickyButton'>See all my wonderful Talents!</button>");
 
     $('body').on("click", "#clickyButton", function(){
         $("#clickyButton").hide();
@@ -8,10 +8,7 @@ $(document).ready(function(){
 
     });
     $('body').on("click", "#content", function(){
-
         $( "#content" ).slideUp( "slow" );
         $("#clickyButton").show();
-
-
     });
 });
